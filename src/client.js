ad35114526a425bf9af6e562b5ef1f68254ccb82
G3N3R4L0 <monden.londen@yahoo.com>
2023-01-04 23:25:23 +0200

    build web

--- src/client.js
+++ src/client.js
@@ -2,12 +2,12 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: "bkzwsx6i",
     dataset: "production",
     apiVersion: "2022-02-01",
     ignoreBrowserTokenWarning: true,
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: "skoj6aIW1Wl47qym4Sg5sUv1VBOqENkehYoExYrBascRdp1aSZPwjWI8JWZrbdc40B6O5iEbaFL5k6LFNSVGOH7OmjqNsbnd64DVjYLyS6gT64Q6egOXylFVbeOJ4i8cEQv42FhhVPgwPKPxQB7qDvkqEhaVBzIRirswSJCAV4Zm2JUrjHit",
 });
 
 const builder = imageUrlBuilder(client);
